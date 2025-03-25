2456bd73b772564871151726ed2034022260c0f6
yujinmon <trixie@o.cnu.ac.kr>
2025-03-25 08:35:24 +0000

    chararr

---
+++ chararr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  int num[5] = {111, 222, 333, 444, 555};
+  char ch[3] = {'c', 'd', 'e'};
+  //float d[3] = {99.9, 88.8, 77.7};
+
+  printf("int: %d\n", num[3]);
+  printf("ch: %c\n", ch[2]);
+  //printf("float: %f\n", d[2]);
+}
