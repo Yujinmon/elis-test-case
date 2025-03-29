72ff301848271fbac2c10f9413c0b618a522a8bd
yujinmon <trixie@o.cnu.ac.kr>
2025-03-29 13:24:02 +0000

    printtest

---
+++ printtest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int a = 2222;
+  int b = 4444;
+  int c = 300;
+  int d = 33;
+  printf("%d\n", a + b);
+  printf("%d + %d = %d\n", c, d, c + d);
+  printf("print test!\n");
+}
