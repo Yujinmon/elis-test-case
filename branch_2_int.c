d187a2a44b1c223a4e3432539dca0ff95371e89b
Yujin <78896558+Yujinmon@users.noreply.github.com>
2025-07-09 21:56:28 +0900

    Create branch_2_int.c

---
+++ branch_2_int.c
@@ -0,0 +1,26 @@
+/*
+ * Branches for testing flow-sensitive analysis.
+ * Author: Sen Ye
+ * Date: 08/11/2013
+ */
+
+#include "aliascheck.h"
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	int *p, *q;
+	// int x, y;
+	int x = argv[1][0] - 48;
+	int y;
+	
+	q = &y;
+	if (x) {
+		p = &x;
+		NOALIAS(p, q);
+	}
+	else {
+		p = &y;
+		MUSTALIAS(p, q);
+	}
+	return 0;
+}
